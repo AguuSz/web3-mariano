7ff49d6b58cab613378446e0ac4c171a6cd61126
AguuSz <agus.sepu2000@hotmail.com>
2023-09-12 17:31:49 -0300

    update

--- src/main/java/ar/edu/iua/iw3/backend/controllers/ProductController.java
+++ src/main/java/ar/edu/iua/iw3/backend/controllers/ProductController.java
@@ -3,6 +3,8 @@ package ar.edu.iua.iw3.backend.controllers;
 import ar.edu.iua.iw3.backend.business.IProductBusiness;
 import ar.edu.iua.iw3.backend.util.IStandardResponseBusiness;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,11 @@ public class ProductController extends BaseRestController {
     @Autowired
     private IStandardResponseBusiness response;
 
-    
+    public ResponseEntity<?> list() {
+        try {
+            return new ResponseEntity<>(productBusiness.getProducts(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
